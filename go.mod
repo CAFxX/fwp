e14ee9fdb6c99a51ed0dd69357b353d35bea7ab5
Carlo Alberto Ferraris <cafxx@mercari.com>
2022-12-09 15:29:16 +0900

    hle detect

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alitto/pond v1.8.2
 	github.com/alphadose/itogami v0.4.0
 	github.com/gammazero/workerpool v1.1.3
+	github.com/intel-go/cpuid v0.0.0-20220614022739-219e067757cb
 	github.com/linux4life798/safetyfast v0.0.0-20220714092746-635f4959443b
 	github.com/panjf2000/ants/v2 v2.6.0
 	golang.org/x/sync v0.1.0
--- mutex_hle.go
+++ mutex_hle.go
@@ -2,6 +2,32 @@
 
 package fwp
 
-import "github.com/linux4life798/safetyfast"
+import (
+	"sync"
 
-type mutex struct{ safetyfast.SpinHLEMutex }
+	"github.com/intel-go/cpuid"
+	"github.com/linux4life798/safetyfast"
+)
+
+var hle = cpuid.HasExtendedFeature(cpuid.HLE)
+
+type mutex struct {
+	m  sync.Mutex
+	ms safetyfast.SpinHLEMutex
+}
+
+func (m *mutex) Lock() {
+	if hle {
+		m.ms.Lock()
+	} else {
+		m.m.Lock()
+	}
+}
+
+func (m *mutex) Unlock() {
+	if hle {
+		m.ms.Unlock()
+	} else {
+		m.m.Unlock()
+	}
+}
